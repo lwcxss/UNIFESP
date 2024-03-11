603f45245927ca67dc758f853af5770a66d88ecc
lucas <lwcxss@gmail.com>
2024-03-11 13:55:02 -0300

    limpeza

--- Projeto_POO-BD/src/entidade/Bicicleta.ts
+++
@@ -1,8 +0,0 @@
-export default class Bike {
-  public id: number | undefined;
-  public nome: string | undefined;
-  
-  getById(id: number): void {
-    throw new Error("Method not implemented.");
-  }
-}
